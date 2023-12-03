f01b798f14701531370da5c643601fac09906b01
Aleksey <shmishek77@gmail.com>
2023-12-04 00:22:39 +0300

    str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def upper(text):
+    """
+    Make all letters big
+    """
     return text.upper()
+
+
+def upper_letter(text):
+    """
+    Make first letter big
+    """
+    return text.title()
